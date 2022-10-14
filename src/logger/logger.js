303ea1b1052b185d9ff7c00a59ba931a2fabf0f8
Aliabbas547 <ab9956317681@gmail.com>
2022-10-14 23:05:35 +0530

    i have changed some file

---
+++ src/logger/logger.js
@@ -0,0 +1,6 @@
+let t2= function welcome(){
+    console.log("welcome to my aplication. I am Ali Abbas and a part of FunctionUp Plutonium cohort ");
+    return "done";
+}
+
+module.exports.myFunction= t2
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,23 +1,14 @@
 const express = require('express');
 const router = express.Router();///test-you
-//importing a custom module
-const xyz = require('../logger')
-//importing external package
-const underscore = require('underscore')
+const t3= require("../logger/logger.js");
+
 
 router.get('/test-me', function (req, res) {
-    //Calling the components of a different custom module
-    console.log("Calling my function ",xyz.myFunction())
-    console.log("The value of the constant is ",xyz.myUrl)
-    //Trying to use an external package called underscore
-    let myArray = ['Akash', 'Pritesh', 'Sabiha']
-    let result = underscore.first(myArray)
-    console.log("The result of underscores examples api is : ", result)
+    console.log(t3.myFunction());
+
     
     res.send('My first ever api!')
 
-    //To be tried what happens if we send multiple response
-    //res.send('My second api!')
 });
 
 module.exports = router;
